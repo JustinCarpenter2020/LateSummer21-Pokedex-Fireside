06a2e3d72a3003b33c0ef224e5584209056a1ee6
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-07-22 16:07:25 -0600

    ready to go

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
-import ValuesController from "./Controllers/ValuesController.js";
+import PokemonController from "./Controllers/PokemonController.js";
 
 class App {
-  valuesController = new ValuesController();
+  pokemonController = new PokemonController();
 }
 
 window["app"] = new App();
